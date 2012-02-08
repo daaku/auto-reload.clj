9620fa9d9bc749ed896223e397e2fb299378a622
Naitik Shah <n@daaku.org>
2012-02-07 18:02:12 -0800

    lock-in snapshot versions, :checksum-deps

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :author "Naitik Shah <n@daaku.org>"
   :url "https://github.com/nshah/auto-reload.clj"
   :repl-init auto-reload.repl
+  :checksum-deps true
+  :exclusions [org.clojure/clojure]
   :dependencies
     [[ns-tracker "0.1.1"]
      [org.clojure/clojure "1.3.0"]]
   :dev-dependencies
-    [[lein-marginalia "0.7.0-SNAPSHOT"]
-     [org.clojure/clojure "1.3.0"]
+    [[lein-marginalia "0.7.0-20111019.122151-1"]
      [org.clojure/tools.logging "0.2.3"]
-     [vimclojure/server "2.3.0-SNAPSHOT"]])
+     [vimclojure/server "2.3.1"]])
